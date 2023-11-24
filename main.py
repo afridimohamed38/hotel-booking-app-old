8843db47b8d98defb9429b401e01f0b7e4671aa3
Afridi <afridimohamed38@gmail.com>
2023-11-24 23:44:09 +0530

    Building basic classes

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pandas as pd
 
-df = pd.read_csv("hotels.csv")
+df = pd.read_csv("hotels.csv", dtype={"id":str})
+
 
 class Hotel:
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        """Book a hotel by changing its availability to no"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        """Check if the hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 
 class ReservationTicket:
@@ -20,12 +28,14 @@ class ReservationTicket:
     def generate(self):
         pass
 
+
 # this below is the programming main loop
 print(df)
-id = input("Enter a hotel id")
+hotel_ID = input("Enter a hotel id: ")
+hotel = Hotel(hotel_ID)
 if hotel.available():
-    hotel = Hotel(id)
-    name = input("Enter your name")
+    name = input("Enter your name: ")
+    hotel.book()
     reservation_ticket = ReservationTicket(name, hotel)
     print(reservation_ticket.generate())
 else:
