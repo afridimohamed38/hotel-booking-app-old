0dec46f2ba8807272c473cc115e46153227048ab
Afridi <afridimohamed38@gmail.com>
2023-11-24 22:52:24 +0530

    Create skeleton structure using main loop

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+import pandas as pd
+
+df = pd.read_csv("hotels.csv")
+
 class Hotel:
+    def __init__(self, id):
+        pass
+
     def book(self):
+        pass
+
+    def available(self):
         pass
 
 
 class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        pass
+
     def generate(self):
         pass
+
+# this below is the programming main loop
+print(df)
+id = input("Enter a hotel id")
+if hotel.available():
+    hotel = Hotel(id)
+    name = input("Enter your name")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("Hotel is not free")
